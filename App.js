dafb9bd0b24e69d66556250fdbd228d5e643d9e5
Tanmaysv <tanmaysvshetty@gmail.com>
2021-04-11 22:35:26 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Main />
+      </View>
+    );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
